d16356c1b20618f171602dc48f10e1f7ceb3d8ad
Ellie <ellie@omnistory.net>
2023-05-12 16:24:46 +0900

    [style] 채팅룸 데모 메인 링크 주석 추가

--- sample/video_conference_sample/lib/main.dart
+++ sample/video_conference_sample/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+// import 'package:sample/screen/demo_chatting_screen.dart';
 import 'package:sample/screen/home_screen.dart';
 
 void main() {
   runApp(const MaterialApp(
     home: HomeScreen(),
+    // home: ChattingDemo(),
   ));
 }
 
